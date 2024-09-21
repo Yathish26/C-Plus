ad11092a28e069d309cb8403e9d7c82f11c03068
Yathish Acharya <yathishkumar2013@gmail.com>
2024-09-22 00:27:39 +0530

    cplus

---
+++ add.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+
+
+int main(){
+    int x;
+    int y;
+    int sum;
+    cout<<"Enter an Number: ";
+    cin>>x;
+    cout<<"Enter Another Number: ";
+    cin>>y;
+    sum = x + y;
+    cout<<"Total is :"<<sum ;
+};
